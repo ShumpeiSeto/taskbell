642e2f079e5ba01377cea6a70f0d7c49cba0360e
Shumpeiseto <shunpei.seto@gmail.com>
2025-06-26 06:56:51 +0900

    add some with flask-login tutorial

--- taskbell/__init__.py
+++ taskbell/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 print("__init__.pyがじっこうされました")
 app = Flask(__name__)
@@ -7,7 +8,8 @@ print("appがつくられました")
 app.config.from_object("taskbell.config")
 
 db = SQLAlchemy(app)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 # taskbell内の__init__ではviewsは定義されてなかったけど、フォルダ内のviews.pyを発見した
 # views.pyを実行する
 from taskbell import views
