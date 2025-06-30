e01d7de6b3445c11d1fe426912819448663d7644
ShumpeiSeto <shunpei.seto@gmail.com>
2025-06-30 15:03:24 +0900

    add Initial Migration

--- taskbell/__init__.py
+++ taskbell/__init__.py
@@ -1,22 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate 
 
 # from .models.login_user import User
 
 print("__init__.pyがじっこうされました")
 app = Flask(__name__)
 print("appがつくられました")
+# config file別途作成している
+
 app.config.from_object("taskbell.config")
 app.secret_key = "abcdefghijk"
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-# # sessionに保存されているuser_idからユーザーオブジェクトを返す
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+
+# Migration 設定
+migrate = Migrate(app, db)
 
 # taskbell内の__init__ではviewsは定義されてなかったけど、フォルダ内のviews.pyを発見した
 # views.pyを実行する
