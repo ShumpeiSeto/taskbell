d28124f5a35fa24cf9464fba75024e23d5e1587e
Shumpeiseto <shunpei.seto@gmail.com>
2025-07-14 07:53:16 +0900

    fix app.run setting for render.com

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 # taskbell内に__init__があればそれを実行してappがあればそれを返す
 from taskbell import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    # Render.comが提供するポートを取得（なければ5000）
+    port = int(os.environ.get("PORT", 5000))
+    # 本番環境用の設定
+    app.run(host="0.0.0.0", port=port, debug=False)
