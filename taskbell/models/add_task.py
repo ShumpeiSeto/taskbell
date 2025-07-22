aafbe60eb8362407d8ee889916c44f05ab035eb2
Shumpeiseto <shunpei.seto@gmail.com>
2025-07-23 06:55:29 +0900

    重要度のバックエンド実装と新規タスク画面修正

--- taskbell/models/add_task.py
+++ taskbell/models/add_task.py
@@ -3,11 +3,19 @@ from datetime import datetime
 
 
 class Tasks(db.Model):
-    def __init__(self, title=None, deadline=None, is_completed=None, user_id=None):
+    def __init__(
+        self,
+        title=None,
+        deadline=None,
+        is_completed=None,
+        user_id=None,
+        importance=None,
+    ):
         self.title = title
         self.deadline = deadline
         self.is_completed = is_completed
         self.user_id = user_id
+        self.importance = importance
 
     __tablename__ = "tasks"
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -15,6 +23,7 @@ class Tasks(db.Model):
     deadline = db.Column(db.DateTime, nullable=True)
     is_completed = db.Column(db.Boolean, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    importance = db.Column(db.Integer, nullable=False)
     created_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_time = db.Column(
         db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
