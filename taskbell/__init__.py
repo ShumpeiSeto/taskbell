ffa67b89ebb5c59db7da223c6d176e334bbcc04e
Shumpeiseto <shunpei.seto@gmail.com>
2025-07-24 17:08:10 +0900

    ボタンとセッションの紐づけ実装

--- taskbell/__init__.py
+++ taskbell/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
