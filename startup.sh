271439b64c5af36e593e87630680280092851501
Shumpeiseto <shunpei.seto@gmail.com>
2025-07-31 22:01:38 +0900

    デプロイ時のデータ消え対策をする

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@ echo "デプロイ開始"
 
 export FLASK_APP=taskbell 
 
-flask db init || true
-flask db migrate || true
+# flask db init || true
+flask db migrate -m "Auto migration" || true
 flask db upgrade
 
 python server.py
