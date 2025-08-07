71e83532cddceb42e93be7d625a5c25b71b69c9a
Shumpeiseto <shunpei.seto@gmail.com>
2025-08-07 18:57:50 +0900

    SessionとSessionStorageの橋渡し実装

--- taskbell/static/script.js
+++ taskbell/static/script.js
@@ -6,16 +6,10 @@ const modal_tasks = document.querySelector(".modal-tasks");
 
 const close_modal = document.querySelector(".close_modal");
 
-// dateオブジェクトに変換用編集関数
-// const slice_date_str = function (date_str) {
-//   const result = "20" + date_str.slice(0, 8);
-//   return result.replaceAll("/", "-");
-// };
-
 intervalId ??= setInterval(checkdatetime, 180000);
 function checkdatetime() {
   // クライアントセッションから取得
-  const dead_minutes = sessionStorage.getItem("dead_minutes");
+  const dl_time = sessionStorage.getItem("dl_time");
   // 親要素で取得した方がアクセスしやすい
   const nctask_items = document.querySelectorAll(".nc-task-item");
   // 比較のため現座日時取得
@@ -39,7 +33,7 @@ function checkdatetime() {
       (new Date(limity_date).getTime() - now.getTime()) / (60 * 1000);
 
     // console.log(limity_date, diff);
-    if (diff <= dead_minutes) {
+    if (diff <= dl_time) {
       const tr = modal_tasks.appendChild(document.createElement("tr"));
       tr.classList.add("limity_task");
       const td_taskname = tr.appendChild(document.createElement("td"));
